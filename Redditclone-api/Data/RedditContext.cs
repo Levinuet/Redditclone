b79a86ad4854f85a6263377eace2cd3039c7b18e
benjamin wiese <benjamin060101@gmail.com>
2024-10-02 15:41:52 +0200

    api virker

---
+++ Redditclone-api/Data/RedditContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data
+{
+    public class RedditContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public RedditContext(DbContextOptions<RedditContext> options) : base(options)
+        {
+        }
+
+    }
+}
+
---
+++ Redditclone-api/Model/Comment.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Model
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public string? Author { get; set; }
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int PostId { get; set; }
+
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public Post? Post { get; set; }
+    }
+}
