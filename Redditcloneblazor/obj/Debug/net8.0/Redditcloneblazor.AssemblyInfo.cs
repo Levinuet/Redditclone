fea2eae6209eeee69f9cf9bfd80c7151ce4aa747
Levi Emkjær <eaa23leej@students.eaaa.dk>
2024-10-09 11:23:54 +0200

    små ting
    :)

--- Redditcloneblazor/obj/Debug/net8.0/Redditcloneblazor.AssemblyInfo.cs
+++ Redditcloneblazor/obj/Debug/net8.0/Redditcloneblazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Redditcloneblazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b79a86ad4854f85a6263377eace2cd3039c7b18e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4e614d26276c52278531d80066ea7d7f242413")]
 [assembly: System.Reflection.AssemblyProductAttribute("Redditcloneblazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Redditcloneblazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
